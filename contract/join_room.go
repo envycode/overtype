fefdfcc7d9038e358b9e38219e449b1e0c824fe5
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2020-12-13 19:01:37 +0700

    fix: param validation on ws connection

--- contract/join_room.go
+++ contract/join_room.go
@@ -12,15 +12,20 @@ func NewRequestJoinRoom(r *http.Request) RequestJoinRoomContract {
 	params := r.URL.Query()
 	code := ""
 	participantId := ""
+	participantName := "Anonymous"
 	if len(params["code"]) > 0 {
-		code = params["source_lang"][0]
+		code = params["code"][0]
 	}
 	if len(params["participant_id"]) > 0 {
-		code = params["participant_id"][0]
+		participantId = params["participant_id"][0]
+	}
+	if len(params["participant_name"]) > 0 {
+		participantName = params["participant_name"][0]
 	}
 	return RequestJoinRoomContract{
-		Code:          code,
-		ParticipantId: participantId,
+		Code:            code,
+		ParticipantId:   participantId,
+		ParticipantName: participantName,
 	}
 }
 
